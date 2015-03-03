b1c5a78e3500f0be8cfdbf43b907565ecc766918
Dave <davidkemp@mycwi.cc>
2015-03-03 13:34:47 -0700

    Initial Add of PROJECT2RusticPear to CH 3.

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,12 @@
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes.dex : \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\BuildConfig.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\CordovaApp.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$attr.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$drawable.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$string.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$xml.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\org\apache\cordova\globalization\Globalization$1.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\org\apache\cordova\globalization\Globalization.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\classes\org\apache\cordova\globalization\GlobalizationError.class \
+C:\Sandbox\webd265\PROJECT2RusticPear\platforms\android\ant-build\dexedLibs\classes-738f6dd79970b558de335d6f953d0434.jar \
